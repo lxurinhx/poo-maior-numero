2d84992b51a2278a552ee25d0ff6c0f11889aceb
Laura Faleiro <99209247+lxurinhx@users.noreply.github.com>
2023-05-31 17:30:15 -0300

    Add files via upload

---
+++ Av3D27Ex08/Av3D27Ex08/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Av3D27Ex08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalc = new System.Windows.Forms.Button();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtnum3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblmaior = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalc
+            // 
+            this.btncalc.Location = new System.Drawing.Point(80, 159);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(84, 23);
+            this.btncalc.TabIndex = 0;
+            this.btncalc.Text = "maior número";
+            this.btncalc.UseVisualStyleBackColor = true;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(81, 60);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 20);
+            this.txtnum1.TabIndex = 1;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(81, 86);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 20);
+            this.txtnum2.TabIndex = 2;
+            // 
+            // txtnum3
+            // 
+            this.txtnum3.Location = new System.Drawing.Point(81, 112);
+            this.txtnum3.Name = "txtnum3";
+            this.txtnum3.Size = new System.Drawing.Size(100, 20);
+            this.txtnum3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "insira 3 números:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "número 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "número 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "número 3:";
+            // 
+            // lblmaior
+            // 
+            this.lblmaior.AutoSize = true;
+            this.lblmaior.Location = new System.Drawing.Point(96, 210);
+            this.lblmaior.Name = "lblmaior";
+            this.lblmaior.Size = new System.Drawing.Size(13, 13);
+            this.lblmaior.TabIndex = 8;
+            this.lblmaior.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 265);
+            this.Controls.Add(this.lblmaior);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnum3);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.btncalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtnum3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblmaior;
+    }
+}
+
---
+++ Av3D27Ex08/Av3D27Ex08/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Av3D27Ex08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            int num1, num2, num3;
+
+            num1 = int.Parse(txtnum1.Text);
+            num2 = int.Parse(txtnum2.Text);
+            num3 = int.Parse(txtnum3.Text);
+
+            if (num1 > num2 && num1 > num3)
+            {
+                lblresult.Text = num1.ToString();
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                lblresult.Text = num2.ToString();
+            }
+            else if (num3 > num1 && num3 > num2)
+            {
+                lblresult.Text = num3.ToString();
+            };
+        }
+    }
+}
